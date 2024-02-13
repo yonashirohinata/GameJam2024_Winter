553ee78a9ab1f7ed5c6190c2a69fd1de9dc3a4b5
hanashiro-tsubasa <138739148+hanashiro-tsubasa@users.noreply.github.com>
2024-02-13 18:51:01 +0900

    アイテムの追加（途中）
    アイテムの追加中です

--- Game/DriveAndAvoid/Object/Item.cpp
+++ Game/DriveAndAvoid/Object/Item.cpp
@@ -1,7 +1,8 @@
 #include "Item.h"
 #include "DxLib.h"
+#include "../Utility/InputControl.h"
 
-Item::Item(int handle) :car_engine(NULL), oil_tank(NULL), tool_box(NULL), location(0.0f)
+Item::Item(int item_type, int handle) :car_engine(NULL), oil_tank(NULL), tool_box(NULL), location(0.0f)
 {
 
 }
@@ -13,9 +14,20 @@ Item::~Item()
 
 //初期化処理
 void Item::Initialize()
-{
-	car_engine = LoadGraph("Resource/images/car_engine.png");
-	oil_tank = LoadGraph("Resource/images/oil_tank.png");
-	tool_box = LoadGraph("Resource/images/tool_box.png");
+{   //出現させるX座標パターンを取得
+	float random_x = (float)(GetRand(4) * 105 + 40);
+	//生成位置の設定
+	location = Vector2D(250.0f, 60.0f);
+	//当たり判定の設定
+	box_size = Vector2D(31.0f, 60.0f);
+	//画像の読み込み
+	
+	
+	
 
+}
+//描画処理
+void Item::Draw() const
+{
+	DrawGraphF(location.x,location.y, car_engine, TRUE);
 }
--- Game/DriveAndAvoid/Scene/GameMainScene.h
+++ Game/DriveAndAvoid/Scene/GameMainScene.h
@@ -3,6 +3,7 @@
 #include "SceneBase.h"
 #include "../Object/Player.h"
 #include "../Object/Enemy.h"
+#include "../Object/Item.h"
 
 class GameMainScene : public SceneBase
 {
@@ -10,12 +11,17 @@ private:
 	int high_score;			//ハイスコア
 	int back_ground;		//背景画像
 	int barrier_image;		//バリア画像
+	int car_engine_image;
+	int oil_tank_image;
+	int tool_box_image;
 	int mileage;			//走行距離
 	int enemy_image[3];		//敵画像
 	int enemy_count[3];		//通りすぎた敵カウント
 	int item_image[3];
+	int item_count[3];
 	Player* player;			//プレイヤー
 	Enemy** enemy;			//敵
+	Item** item;             //アイテム
 
 public:
 	GameMainScene();
@@ -34,4 +40,5 @@ private:
 
 	//当たり判定
 	bool IsHitCheck(Player* p, Enemy* e);
+	bool IsHitCheck_item(Player* p, Item* i);
 };
